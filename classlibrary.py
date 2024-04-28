578f43849ea6138236335d047a55a9d56b7ad67f
aadhimithran <157054776+aadhimithran@users.noreply.github.com>
2024-04-29 00:18:28 +0530

    Add files via upload

---
+++ classlibrary.py
@@ -0,0 +1,21 @@
+class multiplefunctions():
+     def subfileds(): 
+        print("sub-fields AI are:")
+        list=['machine learning','neural network','vision','rootics','speech processing',
+              'natural language processing']
+        for i in list:  
+            print(i)
+        return list 
+    
+     def oddeven():
+        number=int(input("enter the number"))
+        if(number%2==0):
+            print("52452 is even number")
+            message="even number"
+        elif(age<18 ):
+            print("your not elegible for marriage")
+            message="eligible"
+        else:
+            print("52452 is odd number")
+            message="odd number"
+        return message
